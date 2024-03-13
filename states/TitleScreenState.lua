b8884c6c8f909d07cb341ef8d695b0567b130416
Izzy <tellmlaye@gmail.com>
2024-03-14 00:26:54 +0800

    Flappy Bird

---
+++ states/TitleScreenState.lua
@@ -0,0 +1,26 @@
+--[[
+    TitleScreenState Class
+
+    The TitleScreenState is the starting screen of the game, shown on startup. It should
+    display "Press Enter" and also our highest score.
+]]
+
+TitleScreenState = Class{__includes = BaseState}
+
+function TitleScreenState:update(dt)
+    -- transition to countdown when enter/return are pressed
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+function TitleScreenState:render()
+    -- simple UI code
+    love.graphics.setFont(flappyFont)
+    love.graphics.setColor(255/255, 223/255, 0/255, 255/255)
+    love.graphics.printf('Flappy Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press "P" to Pause/Resume the game', 0, 200, VIRTUAL_WIDTH, 'center')
+end
